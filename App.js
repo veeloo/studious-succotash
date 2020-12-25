1ee70e5cf6df905f1d62b307588a1327ae8fe704
christianngouw <christiangouw168@gmail.com>
2020-12-25 17:33:24 +0700

    adding touchablewithoutfeedback widget

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Text, View, Image, TextInput, TouchableOpacity } from "react-native";
+import {
+    Text,
+    View,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Keyboard,
+} from "react-native";
 import styles from "./style/style";
 
 import logo from "./assets/favicon.png";
@@ -31,61 +39,69 @@ export default function App() {
     };
 
     return (
-        <View style={styles.container}>
-            {/* logo section */}
-            <View style={{ alignItems: "center", marginTop: 50 }}>
-                <Image source={logo} style={styles.logo} />
-            </View>
-
-            {/* hero text */}
-            <View style={{ marginTop: 50 }}>
-                <View>
-                    <Text style={styles.title}>Let's sign you in</Text>
+        <TouchableWithoutFeedback
+            onPress={() => {
+                Keyboard.dismiss();
+            }}
+        >
+            <View style={styles.container}>
+                {/* logo section */}
+                <View style={{ alignItems: "center", marginTop: 50 }}>
+                    <Image source={logo} style={styles.logo} />
                 </View>
-                <View style={{ marginTop: 5 }}>
-                    <Text style={styles.text}>
-                        we always have a cup of hot tea and {"\n"}a warm
-                        blanket.
+
+                {/* hero text */}
+                <View style={{ marginTop: 50 }}>
+                    <View>
+                        <Text style={styles.title}>Let's sign you in</Text>
+                    </View>
+                    <View style={{ marginTop: 5 }}>
+                        <Text style={styles.text}>
+                            we always have a cup of hot tea and {"\n"}a warm
+                            blanket.
+                        </Text>
+                    </View>
+                </View>
+
+                {/* input field */}
+                <View style={{ marginTop: 20 }}>
+                    <View style={styles.formGroup}>
+                        <Text>Email</Text>
+                        <TextInput
+                            style={styles.formControl}
+                            keyboardType="email-address"
+                            placeholder="e.g studious.succotash@gmail.com"
+                            onChangeText={(val) => setEmail(val)}
+                        />
+                    </View>
+                    <View style={styles.formGroup}>
+                        <Text>Password</Text>
+                        <TextInput
+                            style={styles.formControl}
+                            secureTextEntry={true}
+                            placeholder="this one is secreat sttt..."
+                            onChangeText={(val) => setPassword(val)}
+                        />
+                    </View>
+                    {/* sign in button */}
+                    <View style={styles.formGroup}>
+                        <TouchableOpacity
+                            style={styles.buttonPrimary}
+                            onPress={signIn}
+                        >
+                            <Text style={styles.buttonPrimaryText}>
+                                Sign In
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                {/* create a new account */}
+                <View>
+                    <Text style={styles.textPrimary}>
+                        Don't have an account ? Sign Up here
                     </Text>
                 </View>
             </View>
-
-            {/* input field */}
-            <View style={{ marginTop: 20 }}>
-                <View style={styles.formGroup}>
-                    <Text>Email</Text>
-                    <TextInput
-                        style={styles.formControl}
-                        keyboardType="email-address"
-                        placeholder="e.g studious.succotash@gmail.com"
-                        onChangeText={(val) => setEmail(val)}
-                    />
-                </View>
-                <View style={styles.formGroup}>
-                    <Text>Password</Text>
-                    <TextInput
-                        style={styles.formControl}
-                        secureTextEntry={true}
-                        placeholder="this one is secreat sttt..."
-                        onChangeText={(val) => setPassword(val)}
-                    />
-                </View>
-                {/* sign in button */}
-                <View style={styles.formGroup}>
-                    <TouchableOpacity
-                        style={styles.buttonPrimary}
-                        onPress={signIn}
-                    >
-                        <Text style={styles.buttonPrimaryText}>Sign In</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            {/* create a new account */}
-            <View>
-                <Text style={styles.textPrimary}>
-                    Don't have an account ? Sign Up here
-                </Text>
-            </View>
-        </View>
+        </TouchableWithoutFeedback>
     );
 }
